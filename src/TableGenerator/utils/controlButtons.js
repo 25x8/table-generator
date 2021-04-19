b18b602c75041d650ed5c5190ff89529edcbe21b
Artem Shovkin <artishock4live@gmail.com>
2021-04-19 18:01:14 +0300

    start create edit button

--- src/TableGenerator/utils/controlButtons.js
+++ src/TableGenerator/utils/controlButtons.js
@@ -1,3 +1,65 @@
+const dataPattern = {
+    id: "new-row",
+    characteristics: [
+        {
+            id: "weight",
+            min: null,
+            max: null,
+            eq: null,
+        },{
+            id: "length",
+            min: null,
+            max: null,
+            eq: null,
+        }
+    ],
+    rules: [
+        {
+            id: "inner_diameter",
+            min: null,
+            max: null,
+            eq: null,
+            grade: "premium"
+        },
+        {
+            id: "outer_diameter",
+            min: null,
+            max: null,
+            eq: null,
+            grade: "premium"
+        },
+        {
+            id: "inner_diameter",
+            min: null,
+            max: null,
+            eq: null,
+            grade: "secondary"
+        },
+        {
+            id: "outer_diameter",
+            min: null,
+            max: null,
+            eq: null,
+            grade: "secondary"
+        },
+        {
+            id: "inner_diameter",
+            min: null,
+            max: null,
+            eq: null,
+            grade: "reject"
+        },
+        {
+            id: "outer_diameter",
+            min: null,
+            max: null,
+            eq: null,
+            grade: "reject"
+        },
+    ]
+}
+
+
 export function createCopyButton(container, store, data, tableObject) {
     const button = document.createElement('button');
     button.classList.add('button-copy');
@@ -14,6 +76,22 @@ export function createDeleteButton(container, store, data, tableObject) {
     container.appendChild(button);
 }
 
+export function createEditButton(container, store, data, rowPattern) {
+    const editButton = document.createElement('button');
+    editButton.classList.add('button-delete');
+    editButton.textContent = 'Редактировать';
+    const index = store.findIndex(el => el.id === data.id);
+    container.appendChild(editButton);
+    editButton.onclick = () => {
+        const saveButton = document.createElement('button');
+        saveButton.classList.add('button-save');
+        saveButton.textContent = 'Сохранить';
+        container.removeChild(editButton);
+        container.appendChild(saveButton);
+    };
+
+}
+
 function copyData(store, data, tableObject) {
     const index = store.findIndex(el => el.id === data.id);
     const newStore = [...store];
@@ -28,3 +106,5 @@ function deleteData(store, data, tableObject) {
     newStore.splice(index, 1);
     tableObject.updateBody(newStore);
 }
+
+
